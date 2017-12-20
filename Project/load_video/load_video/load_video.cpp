3e61054cdfff1ea76c821f6984bd275fad744571
Jhen Yi Ding <jhenyiding@gmail.com>
2017-12-20 12:03:33 +0900

    load_video

---
+++ Project/load_video/load_video/load_video.cpp
@@ -0,0 +1,31 @@
+﻿#include "opencv2\opencv.hpp"
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+
+	string fileName = "pikachu.avi"; //檔案名稱
+	VideoCapture video(fileName); //讀檔案, 若要改成讀取視訊鏡頭的影像串流改成0或是1
+
+	if (!video.isOpened())
+	{
+		return 0;
+	}
+
+	Mat videoFrame;
+	while (true)
+	{
+		video >> videoFrame; //將影像餵給videoFrame
+
+		if (videoFrame.empty())
+		{
+			break;
+		}
+
+		imshow("Video demo", videoFrame); //顯示影像
+		waitKey(22);
+	}
+	return 0;
+}
